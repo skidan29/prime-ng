b69ffec94b6e85ce1899cd43b4a78de91aef4825
Alexander <skidan2908@gmail.com>
2022-08-11 19:16:10 +0300

    UI KIT PrimeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'prime-ng';
+    public value2?: string;
+
+    public onClickedBtn() {
+        this.value2 = this.value2?.toUpperCase();
+        console.log(this.value2);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BadgeModule } from 'primeng/badge';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ButtonModule,
+        BadgeModule,
+        FormsModule,
+        InputTextModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
